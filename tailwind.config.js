a16cdeca1758b1618dfc9916ebb120e39ca4533d
Bilel Sahraoui <bsahraoui.mail@gmail.com>
2023-04-11 14:40:50 +0200

    feat(tailwind_colors): added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#FBF7F7",
+        "secondary": "#F1E9E3",
+        "tertiary": "#EE712B",
+        "quaternary": "#DC4712",
+      }
+    },
   },
   plugins: [],
 }
